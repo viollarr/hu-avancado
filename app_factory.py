dc944e6380ba498481742d329e3fac7ce022d3d6
renzon <renzon@gmail.com>
2018-10-11 14:29:20 -0300

    Criada arquitetura base de blueprints

---
+++ app_factory.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+import settings
+from blueprints.core import bp
+
+
+def create_app(app_settings=None):
+    if app_settings is None:
+        app_settings = settings
+    app = Flask(__name__)
+    app.config.from_object(app_settings)
+    bp.init_app(app)
+    bp.init_app(app, url_prefix='/user')
+    return app
